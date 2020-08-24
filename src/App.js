96329442a672827ce28475bbcd6f499d6fdf8bb1
Svet <svetperun@gmail.com>
2020-08-24 12:18:03 +0200

    Functional version.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 const buttons = document.querySelectorAll("button");
-const amounts = document.querySelectorAll("select");
+const amounts = document.querySelector("select");
 const budget = document.getElementById("budget-amount");
 const profit = document.getElementById("profit");
 const equity = document.getElementById("equity");
 const forward = document.getElementById("forward");
 const divPositions = document.querySelector(".positions");
-// const h3Price = document.querySelector(".actual-price");
+const h3Price = document.querySelector("#actual-price");
+const resetBtn = document.getElementById("reset-btn");
 
 let priceNow;
 let dataFromJSON;
@@ -14,7 +15,7 @@ let endOfData = 8;
 let positions = [];
 let count = 0;
 let idx = endOfData;
-
+let closeAllFlag = 0;
 const randomIndex = (x) => Math.floor(Math.random() * (x - endOfData + 1));
 
 async function fetchData() {
@@ -35,8 +36,9 @@ function randomData() {
 		dataFromJSON = data.slice(startIdx);
 		currentData = dataFromJSON.slice(0, endOfData);
 		priceNow = currentData[idx].close;
-		// h3Price.innerHTML = `${priceNow}$`;
+		h3Price.innerHTML = `${priceNow}$`;
 		chart.data = currentData;
+		reset();
 	});
 }
 randomData();
@@ -88,17 +90,17 @@ lineSeries.strokeOpacity = 0.5;
 // chart.scrollbarX = scrollbarX;
 
 //amChart end
-
-let budgetAmount = 10000;
-budget.innerText = `${budgetAmount}$`;
+const startBudget = 10000;
+let budgetAmount = startBudget;
+budget.innerText = `${budgetAmount.toFixed(2)}$`;
 
 let lot = 1000;
 // let priceNow = data[idx].close;
 // h3Price.innerHTML = `${priceNow}$`;
 let profitAmount = 0;
-profit.innerText = `${profitAmount}$`;
-let equityAmount = 0;
-equity.innerText = `${equityAmount}$`;
+profit.innerText = `0.00$`;
+let equityAmount = budgetAmount;
+equity.innerText = `${budgetAmount.toFixed(2)}$`;
 
 const updateAtForward = () => {
 	if (idx == dataFromJSON.length - 1) {
@@ -111,9 +113,11 @@ const updateAtForward = () => {
 	chart.data = currentData;
 	profitAmount = 0;
 	idx++;
-
+	priceBefore = currentData[idx - 1].close;
 	priceNow = currentData[idx].close;
-	// h3Price.innerHTML = `${priceNow}$`;
+	h3Price.innerHTML = `${priceNow}$`;
+	substract = priceNow - priceBefore;
+	changeColorOf(substract, h3Price);
 	const profitSpans = document.querySelectorAll(".profit-span");
 	profitSpans.forEach((val, i) => {
 		let { price, amount, type } = positions[i];
@@ -127,14 +131,34 @@ const updateAtForward = () => {
 		}
 
 		profit = profit.toFixed(2);
+
 		positions[i].profit = parseFloat(profit);
-		val.innerHTML = profit;
-		profitAmount += parseFloat(profit);
+		val.innerHTML = profit + "$";
+		if (profit > 0) val.classList = "profit-span profit";
+		else if (profit < 0) val.classList = "profit-span loss";
+		else val.classList = "profit-span";
+		profitAmount += parseFloat(profit.slice(0, profit.length - 1));
 	});
 	profit.innerText = profitAmount.toFixed(2) + "$";
+	if (profitAmount > 0) profit.className = "amount profit";
+	if (profitAmount < 0) profit.className = "amount loss";
+	if (profitAmount == 0) profit.className = "amount";
 	updateEquity();
 };
-
+const changeColorOf = (substract, target) => {
+	if (substract > 0) {
+		target.classList.add("profit");
+		setTimeout(() => {
+			target.classList.remove("profit");
+		}, 500);
+	} else if (substract < 0) {
+		target.classList.add("loss");
+		setTimeout(() => {
+			target.classList.remove("loss");
+		}, 500);
+	}
+};
+const removeAllPositions = () => {};
 const newPosition = (id, price, date, amount, type) => {
 	let position = {
 		id: id,
@@ -151,30 +175,40 @@ const newPosition = (id, price, date, amount, type) => {
 
 const deletePosition = (el, id) => {
 	divPositions.removeChild(el);
-
+	let substract = positions.find((val) => val.id == id).profit;
 	updateBudget(id);
 	updateProfit(id);
+	updateEquity();
 
 	positions = positions.filter((val) => val.id != id);
+
+	if (!closeAllFlag) changeColorOf(substract, budget);
 };
 
 const updateBudget = (id) => {
 	budgetAmount += positions.find((val) => val.id == id).profit;
-	budgetAmount = parseFloat(budgetAmount.toFixed(2));
-	budget.innerText = `${budgetAmount}$`;
+
+	budget.innerText = `${budgetAmount.toFixed(2)}$`;
 };
 const updateProfit = (id) => {
 	let prof = positions.find((val) => val.id == id).profit;
 	profitAmount -= prof;
 	profit.innerText = profitAmount.toFixed(2) + "$";
+	console.log(profitAmount);
+	if (profitAmount > 0) profit.className = "amount profit";
+	if (profitAmount < 0) profit.className = "amount loss";
+	if (profitAmount == 0) profit.className = "amount";
 };
 const updateEquity = () => {
 	equityAmount = budgetAmount + profitAmount;
 	equity.innerText = equityAmount.toFixed(2) + "$";
+	if (equityAmount > budgetAmount) equity.className = "amount profit";
+	if (equityAmount < budgetAmount) equity.className = "amount loss";
+	if (equityAmount == budgetAmount) equity.className = "amount";
 };
 const createButton = (id, posDiv) => {
 	let button = document.createElement("button");
-	button.innerText = "X";
+	button.innerText = "x";
 	button.className = "end-position-btn";
 	button.addEventListener("click", () => {
 		deletePosition(posDiv, id);
@@ -188,28 +222,52 @@ const addPosition = (id, price, date, amount, type) => {
 	let posDiv = document.createElement("div");
 	posDiv.className = "position";
 	let htmlPosition = document.createElement("p");
-	htmlPosition.innerHTML = `ID: ${id}&nbsp&nbsp Date: ${date}&nbsp&nbsp Type: ${type}&nbsp&nbsp OpenPrice: ${price}&nbsp&nbsp Amount: ${amount}&nbsp&nbsp Profit: <span class="profit-span">0</span>`;
-	posDiv.appendChild(htmlPosition);
+
+	htmlPosition.innerHTML = `Profit: <span class="profit-span">0$</span>&nbsp&nbsp ID: ${id}&nbsp&nbsp Date: ${date}&nbsp&nbsp Type: ${type}&nbsp&nbsp OpenPrice: ${price}&nbsp&nbsp Amount: ${amount}`;
 	posDiv.appendChild(createButton(id, posDiv));
+	posDiv.appendChild(htmlPosition);
+
 	divPositions.appendChild(posDiv);
 };
 const closeAllPositions = () => {
+	closeAllFlag = 1;
+	let substract = profit.innerText.slice(0, profit.innerText.length - 1);
+
+	console.log(substract);
 	let ids = positions.map((val) => val.id);
 	let divs = document.querySelectorAll(".position");
 	ids.forEach((val, idx) => {
 		deletePosition(divs[idx], val);
 	});
+	profit.className = "amount";
+	closeAllFlag = 0;
+	changeColorOf(Number.parseFloat(substract), budget);
 };
-
+const reset = () => {
+	positions = [];
+	divPositions.innerHTML = "";
+	endOfData = 8;
+	idx = endOfData - 1;
+	currentData = dataFromJSON.slice(0, endOfData);
+	priceNow = currentData[idx].close;
+	h3Price.innerHTML = `${priceNow}$`;
+	chart.data = currentData;
+	budgetAmount = startBudget;
+	budget.innerText = `${budgetAmount.toFixed(2)}$`;
+	profit.innerText = "0.00$";
+	equity.innerText = `${budgetAmount.toFixed(2)}$`;
+	profit.classList = "amount";
+	equity.classList = "amount";
+};
 buttons.forEach((val) => {
 	val.addEventListener("click", (e) => {
 		switch (e.target.id) {
 			case "buy-btn":
-				addPosition(count, priceNow, "", parseInt(amounts[0].value), "buy");
+				addPosition(count, priceNow, "", parseInt(amounts.value), "buy");
 
 				break;
 			case "sell-btn":
-				addPosition(count, priceNow, "", parseInt(amounts[1].value), "sell");
+				addPosition(count, priceNow, "", parseInt(amounts.value), "sell");
 				break;
 			case "forward-btn":
 				updateAtForward();
@@ -220,6 +278,8 @@ buttons.forEach((val) => {
 			case "close-all":
 				closeAllPositions();
 				break;
+			case "reset-btn":
+				reset();
 		}
 		console.log(positions);
 	});
